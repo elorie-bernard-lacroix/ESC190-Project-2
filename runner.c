fc26841c3e935a931d42676d92408efbbd8731b0
Elorie Bernard-Lacroix <eloriel0310@gmail.com>
2023-04-07 12:34:02 -0400

    Merge branch 'main'

--- runner.c
+++ runner.c
@@ -7,19 +7,41 @@ int main()
 {
     struct rgb_img* input_im;
     struct rgb_img* output_im;
-    read_in_img(&input_im, "3x4.bin");
+    read_in_img(&input_im, "6x5.bin");
     calc_energy(input_im, &output_im);
-    write_img(output_im, "3x4Energy.bin");
+    write_img(output_im, "6x5Energy.bin");
 
-    double** best_arr = (double**)malloc(sizeof(double*));
-    dynamic_seam(output_im, best_arr);
+    //double** best_arr = (double**)malloc(sizeof(double*));
+    double* best_arr;
+    dynamic_seam(output_im, &best_arr);
     
-    // print best array
+    //print best array
     for (int i = 0; i < output_im->height; i++){
         for (int j = 0; j < output_im->width; j++){
-            printf("%f ", (*best_arr)[i*(output_im->width)+j]);
+            printf("%f ", (best_arr)[i*(output_im->width)+j]);
         }
         printf("\n");
     }
+
+    int *path;
+    recover_path(best_arr, output_im->height, output_im->width, &path);
+    
+    //print path
+    for(int i = 0; i < output_im->height; i++){
+        printf(" %d,", path[i]);
+    }
+
+    struct rgb_img* seamcut;
+    remove_seam(input_im, &seamcut, path);
+
+    for(int i = 0; i < 3; i++){
+        input_im = seamcut;
+        calc_energy(input_im, &output_im);
+        dynamic_seam(output_im, &best_arr);
+        recover_path(best_arr, output_im->height, output_im->width, &path);
+        remove_seam(input_im, &seamcut, path);
+    }
+
+    write_img(seamcut, "fingerscrossed.bin");
     return 0;
 }
